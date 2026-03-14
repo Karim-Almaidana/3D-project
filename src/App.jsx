8d1efbca3f503915c761cba8b6c13529bd7d8ce6
Karim <kareem20224@gmail.com>
2026-03-14 14:34:58 +0200

    fix github pages assets path

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 import { useEffect } from "react";
-import gradient from "/gradient.png"
+import gradient from "/gradient.png";
 
 export default function App() {
-
-  useEffect(()=>{
-    AOS.init({
-      duration: 1500,
-      once: true,
-    })
-  })
+  useEffect(() => {
+    AOS.init(
+      {
+        duration: 1500,
+        once: true,
+      },
+      [],
+    );
+  });
 
   return (
     <main>
       {/* gradient image */}
-      <img className="absolute top-0 right-0 opacity-60 -z-10" src={gradient} alt="gradient-img" />
+      <img
+        className="absolute top-0 right-0 opacity-60 -z-10"
+        src={gradient}
+        alt="gradient-img"
+      />
       {/* Blur effect */}
       <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%] shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10 "></div>
-        {/* Header */}
+      {/* Header */}
       <Header />
       <Hero />
     </main>
-  )
+  );
 }
