489c83f5c6e0d9436e01e998303b3a5d5ba1575a
Karim <kareem20224@gmail.com>
2026-03-14 14:28:49 +0200

    fix asset path

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from "react";
+import gradient from "/gradient.png"
 
 export default function App() {
 
@@ -16,7 +17,7 @@ export default function App() {
   return (
     <main>
       {/* gradient image */}
-      <img className="absolute top-0 right-0 opacity-60 -z-10" src="/gradient.png" alt="gradient-img" />
+      <img className="absolute top-0 right-0 opacity-60 -z-10" src={gradient} alt="gradient-img" />
       {/* Blur effect */}
       <div className="h-0 w-[40rem] absolute top-[20%] right-[-5%] shadow-[0_0_900px_20px_#e99b63] -rotate-[30deg] -z-10 "></div>
         {/* Header */}
